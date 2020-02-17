f574733d58e86bba78c6d3accaa2ab7a0f91bbe0
System Administrator <root@MacBook-Pro-8.local>
2020-02-17 16:28:15 +0800

    Site updated: 2020-02-17 16:28:15

--- js/src/scroll-cookie.js
+++ js/src/scroll-cookie.js
@@ -1,3 +1,3 @@
-// build time:Sat Feb 15 2020 16:17:03 GMT+0800 (GMT+08:00)
+// build time:Mon Feb 17 2020 16:28:13 GMT+0800 (GMT+08:00)
 $(document).ready(function(){var o=window.location.href.replace(window.location.origin,"");var i;$(window).on("scroll",function(){clearTimeout(i);i=setTimeout(function(){Cookies.set("scroll-cookie",$(window).scrollTop()+"|"+o,{expires:365,path:""})},250)});if(Cookies.get("scroll-cookie")!==undefined){var e=Cookies.get("scroll-cookie").split("|");if(e[1]==o){$(window).scrollTop(e[0])}}});
 //rebuild by neat 
